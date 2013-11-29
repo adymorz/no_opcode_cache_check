e2f8a946a4fb1d495f31213e3eb5ca2c2714604c
Adrian Dymorz <dev@adrian.dymorz.ch>
2013-11-29 22:56:49 +0100

    Corrected dependencies to be able to upload to TER

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,7 +27,7 @@ $EM_CONF[$_EXTKEY] = array (
 		'depends' => 
 		array (
 			'php' => '5.2.0-0.0.0',
-			'typo3' => '6.0.0-0.0.0'
+			'typo3' => '6.0.0-6.2.99'
 		),
 		'conflicts' => '',
 		'suggests' => 
